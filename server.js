a48f132b904fce28bb74ed979d3a4375fef0e8ee
Ľuboš Voška <voska.lubos@gmail.com>
2018-01-12 13:34:54 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+
+var sql = require('mssql');
+
+// Connection string parameters.
+var config = {
+    user: 'UserName',
+    password: 'Password',
+    server: 'MS SQL server',
+    database: 'Database'
+}
+
+// Start server and listen on http://localhost:8000/
+var server = app.listen(8000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Server listening at http://%s:%s", host, port)
+})
+
+
+async function execute(query) {
+
+    return new Promise((resolve, reject) => {
+
+        new sql.ConnectionPool(sqlConfig).connect().then(pool => {
+            return pool.request().query(query)
+        }).then(result => {
+
+            resolve(result.recordset);
+
+            sql.close();
+        }).catch(err => {
+
+            reject(err)
+            sql.close();
+            
+        });
+    });
+
+}
+
+// Get query that return data from orders table
+app.get('/orders', function (req, res) {
+    execute('SELECT * FROM Orders;')
+        .then(function(value) {
+            res.end(JSON.stringify(value)); // Result in JSON format
+        });
+})
+
+// Get query with where
+app.get('/orders/:orderId/', function (req, res) {
+    execute('SELECT * FROM Orders WHERE orderId = ' + req.params.orderId)
+        .then(function(value) {
+            res.end(JSON.stringify(value)); // Result in JSON format
+        });
+})
